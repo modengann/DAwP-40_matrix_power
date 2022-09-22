6150da1fb90976d46499b244d78f6b3dc289f265
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 09:05:45 -0400

    init

---
+++ src/matrix_power.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import numpy as np
+from functools import reduce
+
+def matrix_power(a, n):
+    pass     
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
